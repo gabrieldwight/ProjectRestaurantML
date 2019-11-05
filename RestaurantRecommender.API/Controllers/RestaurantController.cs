edc2ab02965ddb12613aad8ff0dd5645c283cf47
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-05 20:27:37 +0300

    Restaurant controller added

---
+++ RestaurantRecommender.API/Controllers/RestaurantController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using RestaurantRecommender.MLCommon;
+
+namespace RestaurantRecommender.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly PredictionEnginePool<RestaurantRating, RestaurantPrediction> _machine_model;
+
+        public RestaurantController(PredictionEnginePool<RestaurantRating, RestaurantPrediction> machine_model)
+        {
+            _machine_model = machine_model;
+        }
+
+        public ActionResult Recommend_Restaurant(int id)
+        {
+            
+        }
+    }
+}
