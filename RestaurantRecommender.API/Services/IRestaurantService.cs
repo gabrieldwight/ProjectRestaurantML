d1f7bafa6adeb920d5ca928abbea3507fef357e1
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-05 20:28:08 +0300

    restaurant service interface methods

---
+++ RestaurantRecommender.API/Services/IRestaurantService.cs
@@ -0,0 +1,18 @@
+﻿using RestaurantRecommender.MLCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantRecommender.API.Services
+{
+    public interface IRestaurantService
+    {
+        Restaurant Get_Restaurant_Detail(int id);
+        List<Restaurant> Get_All_Restaurants();
+        string Trained_MachineLearning_ModelPath();
+        List<Restaurant> PredictRestaurantList ();
+
+        List<Restaurant> GetBestRestaurants { get; }
+    }
+}
