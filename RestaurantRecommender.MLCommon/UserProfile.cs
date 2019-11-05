7dc63bb750f0a47a600ec46c40e284a058bee4ed
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-05 20:31:04 +0300

    user profile model

---
+++ RestaurantRecommender.MLCommon/UserProfile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantRecommender.MLCommon
+{
+    public class UserProfile
+    {
+        public int UserId { get; set; }
+        public string UserImage { get; set; }
+        public List<(int restaurantId, int restaurantRating)> UserRestaurantRatings { get; set; }
+    }
+}
