bacd762fb62bbfd160224d3b7fb92d846d9bc1c0
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-05 20:29:19 +0300

    implementing the user interface methods

---
+++ RestaurantRecommender.API/Services/UserProfileService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestaurantRecommender.MLCommon;
+
+namespace RestaurantRecommender.API.Services
+{
+    public class UserProfileService : IUserProfileService
+    {
+        private List<UserProfile> _users = new List<UserProfile>();
+        // Encapsulating the list of loaded users from the comma separated value file
+        public List<UserProfile> GetUsers
+        {
+            get
+            {
+                return _users;
+            }
+        }
+
+        // Accessing simulated user details
+        public UserProfile Get_User_details(int id)
+        {
+            foreach (UserProfile userProfile in _users)
+            {
+                if (id.Equals(userProfile.UserId))
+                {
+                    return userProfile;
+                }
+            }
+            return null;
+        }
+
+        // Getting a list of restaurants visited by the user
+        public List<(int restaurantId, int restaurantRating)> Get_User_Visited_Restaurants(int id)
+        {
+            foreach (UserProfile userProfile in _users)
+            {
+                if (id.Equals(userProfile.UserId))
+                {
+                    return userProfile.UserRestaurantRatings;
+                }
+            }
+            return null;
+        }
+    }
+}
