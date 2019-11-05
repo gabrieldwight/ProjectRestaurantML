f842be2c14c8e19c61c0ce64691580902da96de2
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-05 20:28:28 +0300

    user profile interface methods

---
+++ RestaurantRecommender.API/Services/IUserProfileService.cs
@@ -0,0 +1,15 @@
+﻿using RestaurantRecommender.MLCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantRecommender.API.Services
+{
+    public interface IUserProfileService
+    {
+        UserProfile Get_User_details(int id);
+        List<(int restaurantId, int restaurantRating)> Get_User_Visited_Restaurants(int id);
+        List<UserProfile> GetUsers { get; }
+    }
+}
