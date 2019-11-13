c0af04b5ae868dd5adea869a9e4f9770067b5f97
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-13 18:06:18 +0300

    complete method implementation from the interface

--- RestaurantRecommender.API/Services/UserProfileService.cs
+++ RestaurantRecommender.API/Services/UserProfileService.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using RestaurantRecommender.MLCommon;
 
 namespace RestaurantRecommender.API.Services
 {
     public class UserProfileService : IUserProfileService
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+        private static readonly string _restaurant_visited_base_fileName = "Restaurant_Visited_By_Users.csv";
         private List<UserProfile> _users = new List<UserProfile>();
+        
+        public UserProfileService(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+            _users = LoadUserDataSample();
+        }
+        
         // Encapsulating the list of loaded users from the comma separated value file
         public List<UserProfile> GetUsers
         {
@@ -32,16 +43,47 @@ namespace RestaurantRecommender.API.Services
         }
 
         // Getting a list of restaurants visited by the user
-        public List<(int restaurantId, int restaurantRating)> Get_User_Visited_Restaurants(int id)
+        public List<UserProfile> Get_User_Visited_Restaurants(int id)
         {
-            foreach (UserProfile userProfile in _users)
-            {
-                if (id.Equals(userProfile.UserId))
+            return _users.Where(x => x.UserId == id)
+                .Select(x => new UserProfile()
                 {
-                    return userProfile.UserRestaurantRatings;
+                    restaurantId = x.restaurantId,
+                    restaurantRating = x.restaurantRating
+                })
+                .ToList();
+        }
+
+        // Generated random restaurants visited by the user
+        public List<UserProfile> LoadUserDataSample()
+        {
+            List<UserProfile> data_results = new List<UserProfile>();
+            if (!File.Exists(Path.Combine(_hostingEnvironment.ContentRootPath, "Content", _restaurant_visited_base_fileName)))
+            {
+                throw new FileNotFoundException();
+            }
+            else
+            {
+                using (var reader = new StreamReader(Path.Combine(_hostingEnvironment.ContentRootPath, "Content", _restaurant_visited_base_fileName)))
+                {
+                    string headerLine = reader.ReadLine();
+                    string line;
+                    while (!reader.EndOfStream)
+                    {
+                        line = reader.ReadLine();
+                        var values = line.Split(",");
+
+                        data_results.Add(new UserProfile
+                        {
+                            UserId = Convert.ToInt32(values[0]),
+                            UserImage = "https://avatars.dicebear.com/v2/avataaars/seed.svg?options[mouth][]=smile",
+                            restaurantId = Convert.ToInt32(values[1]),
+                            restaurantRating = Convert.ToInt32(values[2])
+                        });
+                    }
                 }
             }
-            return null;
+            return data_results;
         }
     }
 }
