99230f6c4acad3f6f0d52c7e38fef108eec76de1
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-13 18:05:25 +0300

    Further changes in the class to complete the method implementation from the interface class

--- RestaurantRecommender.API/Services/RestaurantService.cs
+++ RestaurantRecommender.API/Services/RestaurantService.cs
@@ -10,54 +10,69 @@ namespace RestaurantRecommender.API.Services
 {
     public class RestaurantService : IRestaurantService
     {
-        public List<Restaurant> _restaurants = new List<Restaurant>(LoadRestaurantData());
-        public readonly static string _machinelearning_modelpath = @"Restaurant_RecommenderModel.zip";
-        private static readonly string restaurant_base_fileName = "SampleRestaurantList.csv";
-        private readonly IHostingEnvironment _hostingEnvironment;
+        public Lazy<List<Restaurant>> _restaurants = new Lazy<List<Restaurant>>(LoadRestaurantData);
+        public static readonly string _machinelearning_modelpath = "Restaurant_RecommenderModel.zip";
+        private static readonly string _restaurant_base_fileName = "SampleRestaurantList.csv";
+        private static IHostingEnvironment _hostingEnvironment;
+        private static readonly Random random = new Random();
         // Constructor to initialize dependency injection for IHosting service
         public RestaurantService(IHostingEnvironment hostingEnvironment)
         {
             _hostingEnvironment = hostingEnvironment;
         }
+
+        // List property to store the best restaurants
         public List<Restaurant> GetBestRestaurants
         {
             get
             {
-                throw new NotImplementedException();
+               return LoadBestRestaurants();
             }
         }
 
+        // To return a list of restaurants based on the filter specified by the user
+        public List<Restaurant> GetBestRestaurantByType(string type)
+        {
+            return _restaurants.Value.Where(x => x.RestaurantType.Equals(type, StringComparison.OrdinalIgnoreCase))
+                .OrderBy(x => random.Next()).Take(7).ToList();
+        }
+
+        // The method used to load any of 7 restaurants that can be used to display the best restaurant
+        public List<Restaurant> LoadBestRestaurants()
+        {
+            return _restaurants.Value.OrderBy(x => random.Next()).Take(7).ToList();
+        }
+
+        // To return a list of all the restaurants created 
         public List<Restaurant> Get_All_Restaurants()
         {
-            return _restaurants;
+            return _restaurants.Value;
         }
 
+        // To return the details of the restaurant accessed
         public Restaurant Get_Restaurant_Detail(int id)
         {
-            return _restaurants.SingleOrDefault(r => r.RestaurantId == id);
+            return _restaurants.Value.SingleOrDefault(r => r.RestaurantId == id);
         }
 
-        public List<Restaurant> PredictRestaurantList()
-        {
-            throw new NotImplementedException();
-        }
-
+        // To get the path of the trained machine learning model
         public string Trained_MachineLearning_ModelPath()
         {
             return Path.Combine(_hostingEnvironment.ContentRootPath, "Models", _machinelearning_modelpath);
         }
 
-        private List<Restaurant> LoadRestaurantData()
+        // To be applied in lazy loading of all the restaurants data
+        private static List<Restaurant> LoadRestaurantData()
         {
             List<Restaurant> restaurant_results = new List<Restaurant>();
             // reading the restaurant csv file
-            if (!File.Exists(Path.Combine(_hostingEnvironment.ContentRootPath, "Content", restaurant_base_fileName)))
+            if (!File.Exists(Path.Combine(_hostingEnvironment.ContentRootPath, "Content", _restaurant_base_fileName)))
             {
                 throw new FileNotFoundException();
             }
             else
             {
-                using (var reader = new StreamReader(Path.Combine(_hostingEnvironment.ContentRootPath, "Content", restaurant_base_fileName)))
+                using (var reader = new StreamReader(Path.Combine(_hostingEnvironment.ContentRootPath, "Content", _restaurant_base_fileName)))
                 {
                     string headerLine = reader.ReadLine();
                     string line;
