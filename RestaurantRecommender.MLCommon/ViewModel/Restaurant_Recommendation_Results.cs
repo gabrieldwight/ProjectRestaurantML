5ff5208f7bdb254b0213af700480767be2fec193
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-13 18:07:58 +0300

    The class to be used to display the machine learning results

---
+++ RestaurantRecommender.MLCommon/ViewModel/Restaurant_Recommendation_Results.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantRecommender.MLCommon.ViewModel
+{
+    public class Restaurant_Recommendation_Results
+    {
+        public string Restaurant_Name { get; set; }
+        public string Restaurant_Type { get; set; }
+        public float Restaurant_Rating { get; set; }
+    }
+}
