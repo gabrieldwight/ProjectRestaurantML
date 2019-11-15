ee2c0c930b1f06ea0277e8e608bd6ed4a14181c4
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-15 04:05:18 +0300

    Settings to be used  for API configuration

---
+++ RestaurantRecommender.Services/Helper/NamedHttpClients.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestaurantRecommender.Services.Helper
+{
+    public static class NamedHttpClients
+    {
+        // Machine Learning for restaurant recommendation
+        public const string Recommend_RestaurantClient = "Recommend_RestaurantClient";
+        public const string Recommend_RestaurantTypeClient = "Recommend_RestaurantTypeClient";
+        public const string Restaurant_VisitedClient = "Restaurant_VisitedClient";
+        public const string UserListClient = "UserListClient";
+        public const string RestaurantListClient = "RestaurantListClient";
+    }
+}
---
+++ RestaurantRecommender.Services/Helper/URLHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace RestaurantRecommender.Services.Helper
+{
+    public static class URLHelper
+    {
+#if DEBUG
+        //public static string IPAddress = DeviceInfo.Platform == DevicePlatform.Android ? "10.0.2.2" : "localhost";
+        public static string BaseEndPoint = "https://restaurantrecommenderapi.azurewebsites.net/";
+#else
+        public const string BaseEndPoint = "https://restaurantrecommenderapi.azurewebsites.net/";
+#endif
+    }
+}
