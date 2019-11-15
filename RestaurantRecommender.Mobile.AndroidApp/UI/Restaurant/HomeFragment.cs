8491887772b504b3a1f3b108dee41e15205e76b3
Gabriel Odero <gabupostiga.gdco@gmail.com>
2019-11-15 03:56:53 +0300

    fragment used to display the list of restaurants in the android application

---
+++ RestaurantRecommender.Mobile.AndroidApp/UI/Restaurant/HomeFragment.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using RestaurantRecommender.Mobile.AndroidApp.Adapter;
+
+namespace RestaurantRecommender.Mobile.AndroidApp.UI.Restaurant
+{
+    public class HomeFragment : Android.Support.V4.App.Fragment
+    {
+        private List<RestaurantSection> restaurantItemModel = new List<RestaurantSection>();
+        private RestaurantSectionAdapter restaurantSectionAdapter;
+        RecyclerView restaurant_recyclerView;
+        RecyclerView.LayoutManager restaurantlayoutManager;
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            // Use this to return your custom view for this Fragment
+            // return inflater.Inflate(Resource.Layout.YourFragment, container, false);
+            var rootview = inflater.Inflate(Resource.Layout.fragment_home, container, false);
+            restaurant_recyclerView = rootview.FindViewById<RecyclerView>(Resource.Id.restaurant_sectioned_recyclerview);
+            restaurantlayoutManager = new LinearLayoutManager(restaurant_recyclerView.Context);
+            restaurant_recyclerView.SetLayoutManager(restaurantlayoutManager);
+            if (Arguments != null)
+            {
+                restaurantItemModel = JsonConvert.DeserializeObject<List<RestaurantSection>>(Arguments.GetString("Restaurant_List"));
+            }           
+            restaurantSectionAdapter = new RestaurantSectionAdapter(restaurantItemModel, restaurant_recyclerView.Context);
+            restaurant_recyclerView.SetAdapter(restaurantSectionAdapter);
+            return rootview;
+            //return base.OnCreateView(inflater, container, savedInstanceState);
+        }
+    }
+}
